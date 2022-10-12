f2ce74a961e383c278346dce2e56e21a3e6e585e
torinunna <torinunna9588@gmail.com>
2022-10-12 22:10:20 +0900

    Initial Commit

---
+++ EmotionDiary/EmotionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDiaryApp.swift
+//  EmotionDiary
+//
+//  Created by YUJIN KWON on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
