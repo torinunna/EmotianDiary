bb86a2fa70a3c8fc9d4bb2ba401257c5a4b76282
torinunna <torinunna9588@gmail.com>
2022-10-20 16:20:22 +0900

    add FocusState to DiaryTextInputView

--- EmotionDiary/Diary Creation/DiaryTextInputView.swift
+++ EmotionDiary/Diary Creation/DiaryTextInputView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct DiaryTextInputView: View {
     
     @ObservedObject var vm: DiaryViewModel
+    @FocusState var focused: Bool
     
     var body: some View {
         VStack {
             TextEditor(text: $vm.text)
+                .focused($focused)
                 .border(.gray.opacity(0.2), width: 2)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.focused = true
+            }
         }
         .padding()
     }
